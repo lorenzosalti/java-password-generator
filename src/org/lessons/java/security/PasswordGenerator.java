cd9d0402ff6cf9ec1180bb9776ccf88f70c069fc
lorenzosalti <lorenzosalti@gmail.com>
2025-06-24 17:27:00 +0200

    feat: variables init with Scanner

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -13,6 +13,24 @@ public class PasswordGenerator {
     byte birthMonth;
     short birthYear;
 
+    System.out.println("Inserire il proprio nome");
+    firstName = in.nextLine();
+
+    System.out.println("Inserire il proprio cognome");
+    lastName = in.nextLine();
+
+    System.out.println("Inserire il proprio colore preferito");
+    favouriteColour = in.nextLine();
+
+    System.out.println("Inserire il proprio giorno di nascita");
+    birthDay = in.nextByte();
+
+    System.out.println("Inserire il proprio mese di nascita");
+    birthMonth = in.nextByte();
+
+    System.out.println("Inserire il proprio anno di nascita");
+    birthYear = in.nextShort();
+
     String resultingPassword;
   }
 }
