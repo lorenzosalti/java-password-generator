0a99c1b6dbc865980097cb201ce79db7205b561c
lorenzosalti <lorenzosalti@gmail.com>
2025-06-24 17:17:29 +0200

    feat: new Scanner instance

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,11 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
   public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
     String firstName;
     String lastName;
     String favouriteColour;
