1dba57450d749aba34cf99b58afd7902bd946681
lorenzosalti <lorenzosalti@gmail.com>
2025-06-24 17:14:05 +0200

    feat: variable decalration

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -2,6 +2,13 @@ package org.lessons.java.security;
 
 public class PasswordGenerator {
   public static void main(String[] args) {
+    String firstName;
+    String lastName;
+    String favouriteColour;
+    byte birthDay;
+    byte birthMonth;
+    short birthYear;
 
+    String resultingPassword;
   }
 }
